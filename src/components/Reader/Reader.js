ba05a753105499b05348c57ea04f1af481eb40d9
TANZEEL MIRZA <33144278+tmtanzeel@users.noreply.github.com>
2019-06-23 21:39:40 +0530

    updated reader

--- src/components/Reader/Reader.js
+++ src/components/Reader/Reader.js
@@ -4,7 +4,7 @@ class Reader extends Component {
   state={
     people: [
       {name: "tanzeel", fruits:["apple", "banana", "grapes"], colors: ["red","salmon","mauve"], car: ["cardilac"], image: "1.png"}
-      //INTETIONALLY COMMENTED OUT FOR SIMPLICITY
+      //INTETIONALLY COMMENTED OUT FOR FOR SOMETIME FOR SIMPLICITY
       // {name: "maximillain", fruits:["pumpkin", "kiwi", "pineapple"], colors: ["pink","cyan","magenta"], car: ["mustang"], image: "2.png"},
       // {name: "daniel", fruits:["custard", "peanuts", "avacado"], colors: ["blue","black","grape vine"], car: ["camaro"], image: "3.png"}
     ],
